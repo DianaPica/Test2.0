73ac33e0c1b6717162abb7b586da972db7f79ced
DianaPica <100901557+DianaPica@users.noreply.github.com>
2022-05-20 00:53:18 +0300

    Add files via upload

---
+++ start-pip-pr-master/src/ro/tuiasi/ac/Teacher.java
@@ -0,0 +1,13 @@
+package ro.tuiasi.ac;
+
+public class Teacher {
+	Teacher(){
+		System.out.println("Ctr. din Teacher");
+	}
+	
+	@Override
+	public String toString() {
+
+		return "from Teacher";
+	}
+}
